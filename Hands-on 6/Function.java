753b3ef2925d3653e32eb07d9e09b7fd8791ac7d
FaithMariana <84046846+FaithMariana@users.noreply.github.com>
2021-06-17 20:30:38 -0500

    Add files via upload

---
+++ Hands-on 6/Function.java
@@ -0,0 +1,30 @@
+package PSO;
+
+class Function {
+
+    /**
+     * Calculate the result of (x^4)-2(x^3).
+     * Domain is (-infinity, infinity).
+     * Minimum is -1.6875 at x = 1.5.
+     * @param x     the x component
+     * @return      the y component
+     */
+    static double functionA (double x) {
+        return Math.pow(x, 4) - 2 * Math.pow(x, 3);
+    }
+
+    /**
+     * Perform Ackley's function.
+     * Domain is [5, 5]
+     * Minimum is 0 at x = 0 & y = 0.
+     * @param x     the x component
+     * @param y     the y component
+     * @return      the z component
+     */
+    static double ackleysFunction (double x, double y) {
+        double p1 = -20*Math.exp(-0.2*Math.sqrt(0.5*((x*x)+(y*y))));
+        double p2 = Math.exp(0.5*(Math.cos(2*Math.PI*x)+Math.cos(2*Math.PI*y)));
+        return p1 - p2 + Math.E + 20;
+    }
+
+}
